4764c4b2b179445d017d4a8deb6fcf4929ce407f
Lincoln-Smith <Lincolnsmith628@gmail.com>
2024-01-23 14:55:31 -0500

    more!

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-//Function to Calc and display Tip, Total bill and amout per person
+//Function to Calc and display Tip, Total bill and amount per person
 function calculateAndDisplay (){
+//get the values from the html form
+let bill = parseFloat(document.getElementById('totalBill').value)
+    let numofPpl = parseInt(document.getElementById('numofPeople').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
+//calculate tip, total Bill and the amount each person owes
+let tip = calculateTip(bill, serviceQuality)
+let totalBill = calculateTotalBill (bill, tip)
+let amtPerPers = calcAmtPerPers (totalBill, numofPpl)
 
-    
+
+//display the results in the html
+document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+document.getElementById('totalBillResult').innerText = 'Total Bill: $' + totalBill.toFixed(2)
+document.getElementById('amtPerPersonResult').innerText = 'Amount per person: $' + amtPerPers.toFixed(2)
 }
